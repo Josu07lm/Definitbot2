50b4241651a6f28a3024f8b419306c3edab27e48
Josu07lm <81869195+Josu07lm@users.noreply.github.com>
2021-05-02 03:53:35 -0500

    Add files via upload

---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚 @${number}. 𝘽𝙞𝙚𝙣𝙫𝙚𝙣𝙞𝙙𝙤 𝙖𝙡 𝙜𝙧𝙪𝙥𝙤${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `Adeus @${number}. Já vai tarde 👋`
+}
+exports.bye = bye
